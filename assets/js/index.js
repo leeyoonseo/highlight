c073e8295b372c00e21635c6e3347f7b7b6d52bc
이윤서 <yoons.lee2019@gmail.com>
2020-03-18 17:38:00 +0900

    데모 index.html highlight 로직 구현 - 특수문자 버그 있으므로 전체 로직 수정 예정

---
+++ assets/js/index.js
@@ -0,0 +1,82 @@
+(function(){
+    'use strict';
+
+    var DATA;
+    var SearchHighlight = window.SearchHighlight;
+    var search = document.getElementById('search');
+
+    ajax();
+
+    function ajax(){
+        var req = new XMLHttpRequest();
+
+        if(!req) return false;
+
+        req.onreadystatechange = function(){
+            if (req.readyState === XMLHttpRequest.DONE) {
+                if(req.status === 200){
+                    DATA = req.responseText;
+
+                    createList(DATA);
+
+                    /* 검색후 highlight */
+                    // var highlightData = SearchHighlight.set(JSON.parse(DATA), '점심');
+                    // createList(highlightData);
+
+                }else{
+                    console.log('Error');
+                }
+            }
+        };
+
+        req.open('GET', 'test.json', true);
+        req.send();
+    }
+
+    /* 대상 List 생성 */
+    function createList(data){
+        if(!data) return false;
+
+        if(typeof data !== 'object'){
+            data = JSON.parse(data);
+        }
+
+        var ulList = document.querySelectorAll('#resultUl'),
+            result = document.getElementById('result'),
+            ulNode,
+            liNode;
+
+        if(ulList.length > 0) {
+            ulList[0].remove();
+        }
+        
+        ulNode = document.createElement('ul');
+        ulNode.id = 'resultUl';
+        
+        for(var i in data){
+            liNode = document.createElement('li');
+            liNode.innerHTML = data[i].subject;
+            
+            ulNode.appendChild(liNode);
+        }
+
+        result.appendChild(ulNode);
+    }
+
+
+    /* 검색 input */
+    search.addEventListener('input', function(e){
+        var word = e.target.value;
+
+        if(word === ''){
+            createList(DATA);
+
+        }else{
+            var highlightData = SearchHighlight.set(JSON.parse(DATA), word);
+            createList(highlightData);
+
+        }
+    });
+
+    
+})();
