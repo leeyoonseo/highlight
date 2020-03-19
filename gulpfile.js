acd1b827a7a14408789f8957673f33072c2658a5
이윤서 <yoons.lee2019@gmail.com>
2020-03-19 17:28:16 +0900

    gulp 셋팅 - minify gitignore node_modules 설정

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+// munufy
+gulp.task('minify', function () {
+    return gulp.src('assets/js/search.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'));
+});
+
+// watch 설정
+gulp.task('watch', function(){
+    gulp.watch('assets/js/search.js', gulp.series('minify'));
+});
+
+// gulp를 실행하면 default 로 concat, watch를 실행하는데 병렬로!
+gulp.task('default', gulp.series('minify', 'watch'));
