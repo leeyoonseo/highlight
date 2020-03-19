ffcbe47187ec8ff8dddb6fe2abc96d56a516e578
이윤서 <yoons.lee2019@gmail.com>
2020-03-19 17:36:42 +0900

    js 네이밍 수정
    - search.js 에서 searchJS

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const rename = require('gulp-rename');
 
 // munufy
 gulp.task('minify', function () {
-    return gulp.src('assets/js/search.js')
+    return gulp.src('assets/js/searchJS.js')
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/js'));
@@ -12,7 +12,7 @@ gulp.task('minify', function () {
 
 // watch 설정
 gulp.task('watch', function(){
-    gulp.watch('assets/js/search.js', gulp.series('minify'));
+    gulp.watch('assets/js/searchJS.js', gulp.series('minify'));
 });
 
 // gulp를 실행하면 default 로 concat, watch를 실행하는데 병렬로!
