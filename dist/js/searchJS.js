5ef43cd66d4d4952fa3b3658563fd318fcd5d2fa
이윤서 <yoons.lee2019@gmail.com>
2020-06-10 15:50:50 +0900

    typescript 옵션 추가

---
+++ dist/js/searchJS.js
@@ -0,0 +1,15 @@
+/**
+ * SearchJS
+ * @author yoonseo.lee <okayoon.lee@gmail.com>
+ * @version 1.0
+ * @since 2020.03
+ * @file 검색 / 검색 하이라이트 js
+ * @example <caption>Setting</caption>
+ * searchJS.setting(DATA, { targetKey : 'subject' });
+ *
+ * @example <caption>Search</caption>
+ * var 반환되는데이터 = searchJS.search('키워드'));
+ */
+(function (global, factory) {
+})(this, function () {
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject('tsconfig.json');
 
-// munufy
+gulp.task('ts', function(){
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('dist/js'));
+});
+gulp.task('default', gulp.series('ts'));
+
+
+// minify
 gulp.task('minify', function () {
-    return gulp.src('assets/js/searchJS.js')
+    return gulp.src('dist/js/searchJS.js')
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/js'));
@@ -12,8 +22,8 @@ gulp.task('minify', function () {
 
 // watch 설정
 gulp.task('watch', function(){
-    gulp.watch('assets/js/searchJS.js', gulp.series('minify'));
+    gulp.watch('assets/js/searchJS.ts', gulp.series('ts', 'minify'));
 });
 
 // gulp를 실행하면 default 로 concat, watch를 실행하는데 병렬로!
-gulp.task('default', gulp.series('minify', 'watch'));
+gulp.task('default', gulp.series('ts', 'minify', 'watch'));
